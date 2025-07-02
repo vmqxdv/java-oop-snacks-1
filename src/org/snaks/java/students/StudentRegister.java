2be3f6d99bc571217c720c3b756bdb24f397bfb2
vmqxdv <vquintilio@gmail.com>
2025-07-02 18:26:40 +0200

    fix: Corrected type of students array in StudentRegister class to Student[]

--- src/org/snaks/java/students/StudentRegister.java
+++ src/org/snaks/java/students/StudentRegister.java
@@ -2,6 +2,11 @@ package org.snaks.java.students;
 
 public class StudentRegister {
 
-  private String[] students;
+  private Student[] students;
   private int studentsAmount;
+
+  public StudentRegister() {
+    this.studentsAmount = 0;
+    this.students = new Student[studentsAmount];
+  }
 }
