5ae6b275d9ba752162838ba8930543ebec3daf46
vmqxdv <vquintilio@gmail.com>
2025-07-02 17:51:45 +0200

    feat: Implement BankAccount class with basic balance management methods

--- src/org/snaks/java/bankAccount/BankAccount.java
+++ src/org/snaks/java/bankAccount/BankAccount.java
@@ -1,5 +1,26 @@
 package org.snaks.java.bankAccount;
 
+import java.math.BigDecimal;
+
 public class BankAccount {
 
+  private int accountCode;
+  private BigDecimal balance;
+
+  public BankAccount(int accountCode) {
+    this.accountCode = accountCode;
+    this.balance = new BigDecimal(0);
+  }
+
+  public BigDecimal getBalance() {
+    return this.balance;
+  }
+
+  public void addBalance(BigDecimal amount) {
+    this.balance.add(amount);
+  }
+
+  public void retriveBalance(BigDecimal amount) {
+    this.balance.subtract(amount);
+  }
 }
